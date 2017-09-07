f758161b530b6fa2eb11a1a5d2e14fcc0d802e8f
Gabriel José Bazán <gbazan@outlook.com>
2017-09-07 00:30:43 -0300

    Include add_resource method

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='simplyrestful',
-    version='0.1.1',
+    version='0.1.2',
     keywords='Simply ReSTful REST API',
     license='MIT',
     platforms='all',
