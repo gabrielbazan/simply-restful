a25ff7c7755169c594908be055b5ee090ac42b09
Gabriel José Bazán <gbazan@outlook.com>
2017-09-17 18:17:28 -0300

    Add query property

--- simplyrestful/database.py
+++ simplyrestful/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 from settings import settings
@@ -11,5 +11,5 @@ print 'Using database: {}'.format(settings['DATABASE'])
 
 engine = create_engine(settings['DATABASE'])
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
+DBSession = scoped_session(sessionmaker(bind=engine))
 session = DBSession()
