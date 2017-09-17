f2ef8b547793419e0bc54fb88eb9ad4b337891cc
Gabriel José Bazán <gbazan@outlook.com>
2017-09-17 18:51:51 -0300

    Add query property

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='simplyrestful',
-    version='0.1.10',
+    version='0.1.11',
     keywords='Simply ReSTful REST API',
     license='MIT',
     platforms='all',
